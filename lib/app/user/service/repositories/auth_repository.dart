a7bc4978deda21a381b7fc98a3654ec2e22bfb0a
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-07-15 21:53:30 -0400

    added base error class

--- lib/app/user/service/repositories/auth_repository.dart
+++ lib/app/user/service/repositories/auth_repository.dart
@@ -1,5 +1,9 @@
+import 'package:crypto_journal_mobile/app/user/service/dtos/auth_payload_dto.dart';
 import 'package:crypto_journal_mobile/app/user/service/dtos/sign_in_dto.dart';
+import 'package:crypto_journal_mobile/shared/errors/base_error.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class AuthRepository {
-  Future signIn(SignInDto signInDto);
+  Future<Either<BaseError, AuthPayloadDto>> signIn(SignInDto signInDto);
+  Future<Either<BaseError, bool>> signOut();
 }
