e663099a0eac5832964d99495bd4e9f975e02a8e
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-07-20 19:04:38 -0400

    implemented network info class

---
+++ test/shared/data/network_info/network_info_test.dart
@@ -0,0 +1,34 @@
+import 'package:crypto_journal_mobile/shared/data/network_info/network_info.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'network_info_test.mocks.dart';
+
+@GenerateMocks([InternetConnectionChecker])
+void main() {
+  late NetworkInfoImpl networkInfoImpl;
+  late MockInternetConnectionChecker mockDataConnectionChecker;
+
+  setUp(() {
+    mockDataConnectionChecker = MockInternetConnectionChecker();
+    networkInfoImpl =
+        NetworkInfoImpl(connectionChecker: mockDataConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test(
+        'should call the hasConnection method of the InternetConnectionChecker class',
+        () async {
+      final tHasConnectionFuture = Future.value(true);
+
+      when(mockDataConnectionChecker.hasConnection)
+          .thenAnswer((_) => tHasConnectionFuture);
+
+      final res = networkInfoImpl.isConnected;
+
+      verify(mockDataConnectionChecker.hasConnection);
+      expect(res, tHasConnectionFuture);
+    });
+  });
+}
