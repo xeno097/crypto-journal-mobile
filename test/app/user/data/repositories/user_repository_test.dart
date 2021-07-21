91254d1e4ad73d12d21558b8e9a8a92ea5f13164
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-07-20 22:25:28 -0400

    started testing user repository

--- test/app/user/data/repositories/user_repository_test.dart
+++ test/app/user/data/repositories/user_repository_test.dart
@@ -1,9 +1,9 @@
 import 'package:crypto_journal_mobile/app/user/data/data_sources/user_remote_data_source.dart';
 import 'package:crypto_journal_mobile/app/user/data/repositories/user_repository.dart';
+import 'package:crypto_journal_mobile/app/user/service/repositories/user_repository.dart';
 import 'package:crypto_journal_mobile/shared/data/network_info/network_info.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
-
 import 'user_repository_test.mocks.dart';
 
 @GenerateMocks([IUserRemoteDataSource, INetworkInfo])
@@ -19,5 +19,16 @@ void main() {
       userRemoteDataSource: userRemoteDataSourceMock,
       networkInfo: networkInfoMock,
     );
+  });
+
+  group('UserRepository', () {
+    test('should be an instance of IUserRepository', () {
+      // arrange
+
+      // act
+
+      // assert
+      expect(userRepository, isA<IUserRepository>());
+    });
   });
 }
