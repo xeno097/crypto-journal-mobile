25ac5241f1470b5efc12619fb4383ccfb42de6d6
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-07-23 20:46:56 -0400

    defined api error classes

---
+++ lib/shared/errors/api_error/api_error.dart
@@ -0,0 +1,26 @@
+import 'package:crypto_journal_mobile/shared/errors/base_error.dart';
+
+class InternalServerError extends BaseError {
+  @override
+  List<Object?> get props => [];
+}
+
+class InvalidUserInputError extends BaseError {
+  @override
+  List<Object?> get props => [];
+}
+
+class UnauthorizedUserError extends BaseError {
+  @override
+  List<Object?> get props => [];
+}
+
+class BlockedUserError extends BaseError {
+  @override
+  List<Object?> get props => [];
+}
+
+class EntityNotFoundError extends BaseError {
+  @override
+  List<Object?> get props => [];
+}
