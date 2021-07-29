064af78bf3a7c37073b2078f6f51e089d418a75a
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-07-28 22:46:04 -0400

    started exporting colors and styles to reuse them

---
+++ lib/shared/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Text colors
+const Color textColorPrimary = Color(0xFFFFFFFF);
+const Color textColorStonks = Color(0xFF37C6BB);
+const Color textColorNotStonks = Color(0xFFC75749);
+
+// Background colors
+const Color backGroundColorPrimary = Color(0xFF2B2D42);
+const Color googleButtonBackGroundColor = Color(0xFFC75749);
+const Color facebookButtonBackGroundColor = Color(0xFF375299);
