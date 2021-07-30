fc982cf43b6b2cccc31f4bc1cc374bcd00348752
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-07-29 20:28:12 -0400

    created history and main dummy pages

---
+++ lib/app/user/presentation/pages/history/history.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+class HistoryPage extends StatelessWidget {
+  const HistoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print("HistoryPage");
+
+    return Container(
+      child: Center(
+        child: Text("History Page"),
+      ),
+    );
+  }
+}
