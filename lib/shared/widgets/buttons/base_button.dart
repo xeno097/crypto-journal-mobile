cd750b3368a83cd047fb86fb8c5751583ce87066
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-07-29 22:35:02 -0400

    fixed base button not passing witdt var to layout container

--- lib/shared/widgets/buttons/base_button.dart
+++ lib/shared/widgets/buttons/base_button.dart
@@ -17,35 +17,32 @@ class BaseButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return LayoutContainer(
-          height: 40.0,
-          child: Container(
-            child: GestureDetector(
-              child: this.child,
-              onTap: this.onTap ?? () {},
-            ),
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black38,
-                  spreadRadius: 2.0,
-                  blurRadius: 4.0,
-                  offset: Offset(
-                    4.0,
-                    4.0,
-                  ),
-                ),
-              ],
-              color: this.backGroundColor,
-              borderRadius: BorderRadius.circular(
-                10.0,
+    return LayoutContainer(
+      height: 40.0,
+      width: this.width,
+      child: Container(
+        child: GestureDetector(
+          child: this.child,
+          onTap: this.onTap ?? () {},
+        ),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black38,
+              spreadRadius: 2.0,
+              blurRadius: 4.0,
+              offset: Offset(
+                4.0,
+                4.0,
               ),
             ),
+          ],
+          color: this.backGroundColor,
+          borderRadius: BorderRadius.circular(
+            10.0,
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
