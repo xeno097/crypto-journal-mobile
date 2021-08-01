5ffe860df422fe6f2219e462ae71d31e29bb9878
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-01 19:19:21 -0400

    created setting tab widget

---
+++ lib/app/user/presentation/pages/settings/widgets/setting_tab.dart
@@ -0,0 +1,78 @@
+import 'package:crypto_journal_mobile/shared/theme/colors.dart';
+import 'package:crypto_journal_mobile/shared/theme/text_styles.dart';
+import 'package:crypto_journal_mobile/shared/widgets/containers/layout_container.dart';
+import 'package:flutter/material.dart';
+
+class SettingTab extends StatelessWidget {
+  final double _settingTabHeight = 50.0;
+  final String title;
+  final List<Widget> children;
+
+  const SettingTab({
+    Key? key,
+    required this.title,
+    required this.children,
+  }) : super(key: key);
+
+  List<Widget> _sectionBuilder() {
+    return this
+        .children
+        .map(
+          (child) => Expanded(
+            child: Container(
+              child: child,
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.all(
+                10.0,
+              ),
+            ),
+          ),
+        )
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 10.0,
+            ),
+            child: Text(
+              this.title,
+              style: defaultTextStyle,
+            ),
+          ),
+          LayoutContainer(
+            height: this.children.length * this._settingTabHeight,
+            child: Container(
+              child: Column(
+                children: this._sectionBuilder(),
+              ),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black38,
+                    spreadRadius: 2.0,
+                    blurRadius: 4.0,
+                    offset: Offset(
+                      4.0,
+                      4.0,
+                    ),
+                  ),
+                ],
+                color: backGroundColorSecondary,
+                borderRadius: BorderRadius.circular(
+                  10.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
