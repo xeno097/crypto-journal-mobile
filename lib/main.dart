44cca5a028fe43573519a3021adc90e89ad97f3a
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-02 22:33:58 -0400

    refactored main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:crypto_journal_mobile/app/user/presentation/pages/splash/splash_screen.dart';
-import 'package:crypto_journal_mobile/shared/routes/routes.dart';
+import 'package:crypto_journal_mobile/app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,19 +7,5 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
-  runApp(ProviderScope(child: MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      routes: appRoutes,
-      initialRoute: SplashScreen.route,
-    );
-  }
+  runApp(ProviderScope(child: CryptoJournalApp()));
 }
