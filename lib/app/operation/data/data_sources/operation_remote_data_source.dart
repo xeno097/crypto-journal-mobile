31dbe697cd55440665c28a3740c27fa042996f42
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-08 14:55:48 -0400

    implemented and tested getOperations method of operation remote data source

--- lib/app/operation/data/data_sources/operation_remote_data_source.dart
+++ lib/app/operation/data/data_sources/operation_remote_data_source.dart
@@ -1,5 +1,28 @@
 import 'package:crypto_journal_mobile/app/operation/data/models/operation_model.dart';
+import 'package:crypto_journal_mobile/shared/data/graphql/graphql_client.dart';
+import 'package:crypto_journal_mobile/shared/data/graphql/operation/queries.dart';
 
 abstract class IOperationRemoteDataSource {
   Future<List<OperationModel>> getOperations();
+}
+
+class OperationRemoteDataSource implements IOperationRemoteDataSource {
+  final IGraphqlClient graphqlAuthClient;
+
+  OperationRemoteDataSource({
+    required this.graphqlAuthClient,
+  });
+
+  @override
+  Future<List<OperationModel>> getOperations() async {
+    final List res = await this.graphqlAuthClient.query(
+          query: GET_OPERATIONS_QUERY,
+          dataKey: GET_OPERATIONS_DATA_KEY,
+        );
+
+    final ret =
+        res.map((operation) => OperationModel.fromJson(operation)).toList();
+
+    return ret;
+  }
 }
---
+++ test/app/operation/data/data_sources/operation_remote_data_source_test.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:crypto_journal_mobile/app/operation/data/data_sources/operation_remote_data_source.dart';
+import 'package:crypto_journal_mobile/shared/data/graphql/graphql_client.dart';
+import 'package:crypto_journal_mobile/shared/data/graphql/operation/queries.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../shared/fixtures/fixture_reader.dart';
+import 'operation_remote_data_source_test.mocks.dart';
+
+@GenerateMocks([IGraphqlClient])
+void main() {
+  late MockIGraphqlClient graphqlAuthClient;
+  late OperationRemoteDataSource operationRemoteDataSource;
+  final operationJson = json.decode(fixtureReader("operation_fixture.json"));
+
+  setUp(() {
+    graphqlAuthClient = MockIGraphqlClient();
+    operationRemoteDataSource = OperationRemoteDataSource(
+      graphqlAuthClient: graphqlAuthClient,
+    );
+  });
+
+  group("OperationRemoteDataSource", () {
+    test('should be a instance of IOperationRemoteDataSource', () {
+      // assert
+      expect(operationRemoteDataSource, isA<IOperationRemoteDataSource>());
+    });
+  });
+
+  group("OperationRemoteDataSource.getOperations", () {
+    setSuccessMocks() {
+      when(graphqlAuthClient.query(
+        dataKey: anyNamed('dataKey'),
+        query: anyNamed('query'),
+        variables: anyNamed('variables'),
+      )).thenAnswer((_) => Future.value([
+            operationJson,
+            operationJson,
+          ]));
+    }
+
+    test('should call the query method of the IGraphqlClient class', () async {
+      // arrange
+      setSuccessMocks();
+
+      // act
+      await operationRemoteDataSource.getOperations();
+
+      // assert
+      verify(graphqlAuthClient.query(
+        query: GET_OPERATIONS_QUERY,
+        dataKey: GET_OPERATIONS_DATA_KEY,
+      ));
+    });
+  });
+}
