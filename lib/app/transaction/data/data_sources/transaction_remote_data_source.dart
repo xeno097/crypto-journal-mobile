eb9a5ec96967d764899f79c05f214fdbabfe2ba0
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-08 23:16:33 -0400

    implemented and tested createTransaction method of transaction remote data source class

--- lib/app/transaction/data/data_sources/transaction_remote_data_source.dart
+++ lib/app/transaction/data/data_sources/transaction_remote_data_source.dart
@@ -1,6 +1,7 @@
 import 'package:crypto_journal_mobile/app/transaction/data/inputs/create_transaction_input.dart';
 import 'package:crypto_journal_mobile/app/transaction/data/models/transaction_model.dart';
 import 'package:crypto_journal_mobile/shared/data/graphql/graphql_client.dart';
+import 'package:crypto_journal_mobile/shared/data/graphql/transaction/mutations.dart';
 import 'package:crypto_journal_mobile/shared/data/graphql/transaction/queries.dart';
 
 abstract class ITransactionRemoteDataSource {
@@ -21,9 +22,19 @@ class TransactionRemoteDataSource implements ITransactionRemoteDataSource {
 
   @override
   Future<TransactionModel> createTransaction(
-      CreateTransactionInput createTransactionDto) {
-    // TODO: implement createTransaction
-    throw UnimplementedError();
+    CreateTransactionInput createTransactionInput,
+  ) async {
+    final Map<String, dynamic> res = await this._graphqlAuthClient.mutate(
+      mutation: CREATE_TRANSACTION_MUTATION,
+      dataKey: CREATE_TRANSACTION_DATA_KEY,
+      variables: {
+        "input": createTransactionInput.toJson(),
+      },
+    );
+
+    final ret = TransactionModel.fromJson(res);
+
+    return ret;
   }
 
   @override
@@ -34,7 +45,9 @@ class TransactionRemoteDataSource implements ITransactionRemoteDataSource {
         );
 
     final ret = res
-        .map((transaction) => TransactionModel.fromJson(transaction))
+        .map(
+          (transaction) => TransactionModel.fromJson(transaction),
+        )
         .toList();
 
     return ret;
--- test/app/transaction/data/data_sources/transaction_remote_data_source_test.dart
+++ test/app/transaction/data/data_sources/transaction_remote_data_source_test.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 
 import 'package:crypto_journal_mobile/app/transaction/data/data_sources/transaction_remote_data_source.dart';
+import 'package:crypto_journal_mobile/app/transaction/data/inputs/create_transaction_input.dart';
 import 'package:crypto_journal_mobile/app/transaction/data/models/transaction_model.dart';
 import 'package:crypto_journal_mobile/shared/data/graphql/graphql_client.dart';
+import 'package:crypto_journal_mobile/shared/data/graphql/transaction/mutations.dart';
 import 'package:crypto_journal_mobile/shared/data/graphql/transaction/queries.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -19,6 +21,15 @@ void main() {
       json.decode(fixtureReader("transactions/transaction_fixture.json"));
 
   final transactionModel = TransactionModel.fromJson(transactionJson);
+
+  final createTransactionInput = CreateTransactionInput(
+    coinSymbol: "ETH",
+    coins: 1000,
+    fee: 0,
+    coinPrice: 3709,
+    date: "12/02/2020",
+    operation: "1",
+  );
 
   setUp(() {
     graphqlAuthClient = MockIGraphqlClient();
@@ -78,6 +89,51 @@ void main() {
         dataKey: GET_TRANSACTIONS_DATA_KEY,
       ));
       expect(res, expectedResult);
+    });
+  });
+
+  group("TransactionRemoteDataSource.createTransaction", () {
+    setSuccessMocks() {
+      when(graphqlAuthClient.mutate(
+        dataKey: anyNamed('dataKey'),
+        mutation: anyNamed('mutation'),
+        variables: anyNamed('variables'),
+      )).thenAnswer((_) => Future.value(transactionJson));
+    }
+
+    test('should call the mutate method of the IGraphqlClient class', () async {
+      // arrange
+      setSuccessMocks();
+
+      // act
+      await transactionRemoteDataSource
+          .createTransaction(createTransactionInput);
+
+      // assert
+      verify(graphqlAuthClient.mutate(
+        mutation: CREATE_TRANSACTION_MUTATION,
+        dataKey: CREATE_TRANSACTION_DATA_KEY,
+        variables: {"input": createTransactionInput.toJson()},
+      ));
+    });
+
+    test('should return the created TransactionModel', () async {
+      // arrange
+      setSuccessMocks();
+
+      // act
+      final res = await transactionRemoteDataSource
+          .createTransaction(createTransactionInput);
+
+      // assert
+      verify(graphqlAuthClient.mutate(
+        mutation: CREATE_TRANSACTION_MUTATION,
+        dataKey: CREATE_TRANSACTION_DATA_KEY,
+        variables: {
+          "input": createTransactionInput.toJson(),
+        },
+      ));
+      expect(res, transactionModel);
     });
   });
 }
