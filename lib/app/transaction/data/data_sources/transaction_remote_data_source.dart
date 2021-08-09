ff51a55b378c05dde94950d5faab5c8bb6420684
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-08 23:04:21 -0400

    implemented and tested getTransaction method of transaction rmeote data source class

--- lib/app/transaction/data/data_sources/transaction_remote_data_source.dart
+++ lib/app/transaction/data/data_sources/transaction_remote_data_source.dart
@@ -1,9 +1,42 @@
 import 'package:crypto_journal_mobile/app/transaction/data/inputs/create_transaction_input.dart';
 import 'package:crypto_journal_mobile/app/transaction/data/models/transaction_model.dart';
+import 'package:crypto_journal_mobile/shared/data/graphql/graphql_client.dart';
+import 'package:crypto_journal_mobile/shared/data/graphql/transaction/queries.dart';
 
 abstract class ITransactionRemoteDataSource {
   Future<List<TransactionModel>> getTransactions();
   Future<TransactionModel> createTransaction(
     CreateTransactionInput createTransactionDto,
   );
+}
+
+class TransactionRemoteDataSource implements ITransactionRemoteDataSource {
+  late final IGraphqlClient _graphqlAuthClient;
+
+  TransactionRemoteDataSource({
+    required graphqlAuthClient,
+  }) {
+    this._graphqlAuthClient = graphqlAuthClient;
+  }
+
+  @override
+  Future<TransactionModel> createTransaction(
+      CreateTransactionInput createTransactionDto) {
+    // TODO: implement createTransaction
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<TransactionModel>> getTransactions() async {
+    final List res = await this._graphqlAuthClient.query(
+          query: GET_TRANSACTIONS_QUERY,
+          dataKey: GET_TRANSACTIONS_DATA_KEY,
+        );
+
+    final ret = res
+        .map((transaction) => TransactionModel.fromJson(transaction))
+        .toList();
+
+    return ret;
+  }
 }
