0b962c6e48d412786a0cb5d1a8ea1873d921d291
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-26 19:47:34 -0400

    implemented and tested the holding service class

--- lib/app/holding/service/services/holding_service.dart
+++ lib/app/holding/service/services/holding_service.dart
@@ -1,5 +1,6 @@
 import 'package:crypto_journal_mobile/app/holding/service/dtos/get_holdings_dto.dart';
 import 'package:crypto_journal_mobile/app/holding/service/dtos/holding_dto.dart';
+import 'package:crypto_journal_mobile/app/holding/service/repositories/holding_repository.dart';
 import 'package:crypto_journal_mobile/shared/errors/base_error.dart';
 import 'package:dartz/dartz.dart';
 
@@ -7,4 +8,21 @@ abstract class IHoldingService {
   Future<Either<BaseError, List<HoldingDto>>> getHoldings({
     required GetHoldingsDto getHoldingsDto,
   });
+}
+
+class HoldingService implements IHoldingService {
+  final IHoldingRepository _holdingRepository;
+
+  HoldingService({
+    required IHoldingRepository holdingRepository,
+  }) : this._holdingRepository = holdingRepository;
+
+  @override
+  Future<Either<BaseError, List<HoldingDto>>> getHoldings({
+    required GetHoldingsDto getHoldingsDto,
+  }) async {
+    return await this
+        ._holdingRepository
+        .getHoldings(getHoldingsDto: getHoldingsDto);
+  }
 }
