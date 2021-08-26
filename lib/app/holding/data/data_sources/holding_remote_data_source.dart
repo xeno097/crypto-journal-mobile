c3e8b32fdda0936f0ad1faa5ae3cc89e02bf8779
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-26 19:25:13 -0400

    implemented and tested the holding remote data source class

--- lib/app/holding/data/data_sources/holding_remote_data_source.dart
+++ lib/app/holding/data/data_sources/holding_remote_data_source.dart
@@ -1,8 +1,36 @@
+import 'package:crypto_journal_mobile/app/holding/data/graphql/queries.dart';
 import 'package:crypto_journal_mobile/app/holding/data/inputs/get_holding_input.dart';
 import 'package:crypto_journal_mobile/app/holding/data/models/holding_model.dart';
+import 'package:crypto_journal_mobile/shared/data/graphql/graphql_client.dart';
 
 abstract class IHoldingRemoteDataSource {
   Future<List<HoldingModel>> getHoldings({
     required GetHoldingInput getHoldingInput,
   });
+}
+
+class HoldingRemoteDataSource implements IHoldingRemoteDataSource {
+  final IGraphqlClient _graphqlAuthClient;
+
+  HoldingRemoteDataSource({
+    required IGraphqlClient graphqlAuthClient,
+  }) : this._graphqlAuthClient = graphqlAuthClient;
+
+  @override
+  Future<List<HoldingModel>> getHoldings({
+    required GetHoldingInput getHoldingInput,
+  }) async {
+    final List res = await this._graphqlAuthClient.query(
+          query: GET_SELF_HOLDINGS_QUERY,
+          dataKey: GET_SELF_HOLDINGS_DATA_KEY,
+        );
+
+    final ret = res
+        .map(
+          (holding) => HoldingModel.fromJson(holding),
+        )
+        .toList();
+
+    return ret;
+  }
 }
