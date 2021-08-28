b8d95ce3f876f996901872a83c52191b5fde1a31
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-27 20:55:55 -0400

    implemented and tested balance remote data source

--- lib/app/balance/data/data_sources/balance_remote_data_source.dart
+++ lib/app/balance/data/data_sources/balance_remote_data_source.dart
@@ -1,8 +1,32 @@
+import 'package:crypto_journal_mobile/app/balance/data/graphql/queries.dart';
 import 'package:crypto_journal_mobile/app/balance/data/inputs/get_balance_input.dart';
 import 'package:crypto_journal_mobile/app/balance/data/models/balance_model.dart';
+import 'package:crypto_journal_mobile/shared/data/graphql/graphql_client.dart';
 
 abstract class IBalanceRemoteDataSource {
   Future<BalanceModel> getBalance({
     required GetBalanceInput getBalanceInput,
   });
+}
+
+class BalanceRemoteDataSource implements IBalanceRemoteDataSource {
+  final IGraphqlClient _graphqlAuthClient;
+
+  BalanceRemoteDataSource({
+    required IGraphqlClient graphqlAuthClient,
+  }) : this._graphqlAuthClient = graphqlAuthClient;
+
+  @override
+  Future<BalanceModel> getBalance({
+    required GetBalanceInput getBalanceInput,
+  }) async {
+    final res = await this._graphqlAuthClient.query(
+          query: GET_SELF_BALANCE_QUERY,
+          dataKey: GET_SELF_BALANCE_DATA_KEY,
+        );
+
+    final ret = BalanceModel.fromJson(res);
+
+    return ret;
+  }
 }
