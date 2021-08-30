e056b2365df85efef11be34db40138da82482ac6
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-29 20:03:43 -0400

    created theme constants file

---
+++ lib/shared/theme/constants.dart
@@ -0,0 +1,9 @@
+const double defaultHeight = 51.0;
+const double defaultWidth = 369.0;
+const double balanceCardHeight = 150.0;
+
+const double secondaryTextStyleSize = 12.0;
+const double primaryTextStyleSize = 15.0;
+const double subTitleTextStyleSize = 18.0;
+const double pageTitleTextStyleSize = 21.0;
+const double mainTitleTextStyleSize = 30.0;
