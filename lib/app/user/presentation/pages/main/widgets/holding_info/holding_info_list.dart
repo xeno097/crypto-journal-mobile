0882d02fd7492b2dc6cf49f21829c4cd03da9663
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-08-31 22:35:09 -0400

    removed hardcoded padding

--- lib/app/user/presentation/pages/main/widgets/holding_info/holding_info_list.dart
+++ lib/app/user/presentation/pages/main/widgets/holding_info/holding_info_list.dart
@@ -1,5 +1,6 @@
 import 'package:crypto_journal_mobile/app/holding/presentation/providers/get_holdings_provider.dart';
 import 'package:crypto_journal_mobile/app/holding/service/dtos/holding_dto.dart';
+import 'package:crypto_journal_mobile/shared/theme/constants.dart';
 import 'package:crypto_journal_mobile/shared/widgets/headers/section_header.dart';
 import 'package:crypto_journal_mobile/app/user/presentation/pages/main/widgets/holding_info/holding_info_list_tile.dart';
 import 'package:crypto_journal_mobile/shared/widgets/loading/default_circular_progress_indicator.dart';
@@ -15,7 +16,9 @@ class HoldingInfoList extends StatelessWidget {
     List<Widget> ret = holdings
         .map(
           (holding) => Padding(
-            padding: const EdgeInsets.only(bottom: 12.0),
+            padding: const EdgeInsets.only(
+              bottom: defaultListElementPadding,
+            ),
             child: HoldingInfoListTile(
               holdingDto: holding,
             ),
