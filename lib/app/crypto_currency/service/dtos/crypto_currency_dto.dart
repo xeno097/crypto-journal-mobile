b968d0b6f5029861ba4049871f0ca6688a5ec182
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-09-03 16:40:32 -0400

    defiend crypto currency repository interface

---
+++ lib/app/crypto_currency/service/dtos/crypto_currency_dto.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+class CryptoCurrencyDto extends Equatable {
+  final String id;
+  final String name;
+  final String symbol;
+  final double price;
+  final String icon;
+
+  CryptoCurrencyDto({
+    required this.id,
+    required this.name,
+    required this.symbol,
+    required this.price,
+    required this.icon,
+  });
+
+  @override
+  List<Object?> get props => [
+        this.id,
+        this.name,
+        this.symbol,
+        this.price,
+        this.icon,
+      ];
+}
