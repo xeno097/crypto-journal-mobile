a45be1b006c7c0c3470e38590853c863df591128
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-09-09 21:59:17 -0400

    created network icon image widget

--- lib/app/transaction/presentation/pages/create_transaction/widgets/search_crypto_currency/search_crypto_currency_list_tile.dart
+++ lib/app/transaction/presentation/pages/create_transaction/widgets/search_crypto_currency/search_crypto_currency_list_tile.dart
@@ -1,10 +1,9 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:crypto_journal_mobile/app/crypto_currency/service/dtos/crypto_currency_dto.dart';
 import 'package:crypto_journal_mobile/shared/theme/constants.dart';
 import 'package:crypto_journal_mobile/shared/theme/text_styles.dart';
 import 'package:crypto_journal_mobile/shared/widgets/containers/base_layout_container.dart';
 import 'package:crypto_journal_mobile/shared/widgets/containers/base_tile_widget.dart';
-import 'package:crypto_journal_mobile/shared/widgets/loading/default_circular_progress_indicator.dart';
+import 'package:crypto_journal_mobile/shared/widgets/images/network_icon_image.dart';
 import "package:flutter/material.dart";
 
 class SelectCyptoCurrencyListTile extends StatelessWidget {
@@ -15,32 +14,9 @@ class SelectCyptoCurrencyListTile extends StatelessWidget {
     required this.cryptoCurrencyDto,
   }) : super(key: key);
 
-  Widget _iconBuilder(double iconSize) {
-    if (this.cryptoCurrencyDto.icon != null) {
-      return CachedNetworkImage(
-        imageUrl: this.cryptoCurrencyDto.icon!,
-        height: iconSize,
-        width: iconSize,
-        placeholder: (context, url) => DefaultCircularProgressIndicator(),
-        errorWidget: (context, url, err) => Icon(
-          Icons.monetization_on_outlined,
-          size: iconSize,
-          color: Colors.white,
-        ),
-      );
-    }
-
-    return Icon(
-      Icons.monetization_on_outlined,
-      size: iconSize,
-      color: Colors.white,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return BaseLayoutContainer(
-      // heigthProp: buttonHeight,
       builder: (context, size) {
         final height = size.height;
         final padding = height * (defaultContainerPadding / defaultHeight);
@@ -50,7 +26,11 @@ class SelectCyptoCurrencyListTile extends StatelessWidget {
           leading: Row(
             mainAxisSize: MainAxisSize.max,
             children: [
-              this._iconBuilder(iconSize),
+              NetworkIconImage(
+                url: this.cryptoCurrencyDto.icon,
+                placeholder: Icons.monetization_on_outlined,
+                size: iconSize,
+              ),
               Padding(
                 padding: const EdgeInsets.only(
                   left: defaultRowItemPadding,
