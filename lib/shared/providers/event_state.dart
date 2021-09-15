8273af08f4274eec1e65f2729ef6fa7b0ea620b0
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-09-14 22:23:28 -0400

    updated event state class to extend from equatable as needed by the riverpod package

--- lib/shared/providers/event_state.dart
+++ lib/shared/providers/event_state.dart
@@ -1,11 +1,18 @@
 import 'package:crypto_journal_mobile/shared/errors/base_error.dart';
+import 'package:equatable/equatable.dart';
 
-abstract class EventState {}
+abstract class EventState extends Equatable {}
 
-class InitialEventState extends EventState {}
+class InitialEventState extends EventState {
+  @override
+  List<Object?> get props => [];
+}
 
 class SuccessEventState extends EventState {
   final String message;
+
+  @override
+  List<Object?> get props => [this.message];
 
   SuccessEventState({
     this.message = "Success",
@@ -14,6 +21,9 @@ class SuccessEventState extends EventState {
 
 class ErrorEventState extends EventState {
   final BaseError error;
+
+  @override
+  List<Object?> get props => [this.error];
 
   ErrorEventState({
     required this.error,
