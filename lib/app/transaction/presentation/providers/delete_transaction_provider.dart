38ef36de23b98e6037a7cae2b009c0cda699c4e6
Sebastiano Faiella <xeno097.cp@gmail.com>
2021-09-19 19:51:22 -0400

    added logic to recreate a transaction after deletion

--- lib/app/transaction/presentation/providers/delete_transaction_provider.dart
+++ lib/app/transaction/presentation/providers/delete_transaction_provider.dart
@@ -1,5 +1,9 @@
+import 'package:crypto_journal_mobile/app/transaction/presentation/providers/create_transaction_provider.dart';
+import 'package:crypto_journal_mobile/app/transaction/service/dtos/create_transaction_dto.dart';
 import 'package:crypto_journal_mobile/app/transaction/service/dtos/delete_transaction_dto.dart';
+import 'package:crypto_journal_mobile/app/transaction/service/dtos/transaction_dto.dart';
 import 'package:crypto_journal_mobile/app/transaction/service/services/transaction_service.dart';
+import 'package:crypto_journal_mobile/shared/classes/call_back_action.dart';
 import 'package:crypto_journal_mobile/shared/providers/functions/handle_provider_error_result.dart';
 import 'package:crypto_journal_mobile/shared/providers/functions/handle_provider_success_result.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -14,9 +18,13 @@ final deleteTransactionProvider = FutureProvider.autoDispose
   ProviderReference ref,
   DeleteTransactionDto deleteTransactionDto,
 ) async {
-  final transactionService = await ref.read(transactionServiceProvider.future);
+  final transactionService = await ref.read(
+    transactionServiceProvider.future,
+  );
 
-  final res = await transactionService.deleteTransaction(deleteTransactionDto);
+  final res = await transactionService.deleteTransaction(
+    deleteTransactionDto,
+  );
 
   return res.fold(
     (err) => handleProviderErrorResult<DeleteTransactionResult>(
@@ -24,9 +32,33 @@ final deleteTransactionProvider = FutureProvider.autoDispose
       err,
       callback: () => DeleteTransactionResult.TransactionNotDeleted,
     ),
-    (deletedTransaction) => handleProviderSuccessResult(
+    (deletedTransaction) =>
+        handleProviderSuccessResult<DeleteTransactionResult>(
       ref,
       () => DeleteTransactionResult.DeletedTransaction,
+      action: CallBackAction(
+        label: "Undo",
+        callback: () async => await _createDeletedTransaction(
+          ref,
+          deletedTransaction,
+        ),
+      ),
     ),
   );
 });
+
+Future _createDeletedTransaction(
+    ProviderReference ref, TransactionDto transaction) async {
+  final CreateTransactionDto createTransactionDto = CreateTransactionDto(
+    coinSymbol: transaction.coinSymbol,
+    coins: transaction.coins,
+    fee: transaction.fee,
+    coinPrice: transaction.cost - transaction.fee,
+    date: transaction.date,
+    operation: transaction.operation.id,
+  );
+
+  await ref.read(createTransactionProvider(
+    createTransactionDto,
+  ).future);
+}
